c90b17bf956483e4901f16c2dca2fb8119699c5d
juanlozano2000 <juaniloza83@gmail.com>
2021-06-16 17:03:42 -0300

    Tarea de clase 1

---
+++ Clase-1/clase_1.js
@@ -0,0 +1,8 @@
+// Le pido su nombre y apellido
+let nombre = prompt("Escriba su nombre");
+
+// Le pido apellido
+let apellido = prompt("Hola " + nombre + " y su apellido?");
+
+alert("Bienvenido al nuevo curso de JS " + nombre + " " + apellido);
+
