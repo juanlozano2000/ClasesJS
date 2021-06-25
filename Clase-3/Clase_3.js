384693fcfef8e724f528a8bef62e7e0b60d570f0
juanlozano2000 <juaniloza83@gmail.com>
2021-06-25 18:45:07 -0300

    desafio clase 3

---
+++ Clase-3/Clase_3.js
@@ -0,0 +1,28 @@
+// //Ejemplo 1
+// // Le pido un numero
+// let numero1 = parseInt(prompt("Ingresa un numero"));
+
+// // Que sume ese numero1 con 1 al 10
+// for (let i = 1; i <=10; i++) {
+// let suma = numero1 + i;
+// alert(numero1 + "+" + i + " = " + suma);
+// }
+
+
+// Ejemplo 2
+// le pido un texto
+let ingresa = prompt("Ingresa tu nombre");
+
+while (ingresa != "ESC"){
+    console.log("Usted ingreso: " + ingresa);
+
+    ingresa = prompt("Ingresa tu nombre");
+}
+
+// Ejemplo 3
+// El numero ingresado quiero que me multicplique los hola
+// let nume = Number(prompt("Ingresa un numero"));
+
+// for(let i = 1; i<=nume;i++){
+//     console.log("Hola");
+// }
